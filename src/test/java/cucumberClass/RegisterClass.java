4462c8b2ddfba31b7739dc8df7d98b05eea49a61
PrakashVenkatachalam77 <prakashv0898@gmail.com>
2024-09-27 13:18:14 +0530

    Cucumber code

---
+++ src/test/java/cucumberClass/RegisterClass.java
@@ -0,0 +1,44 @@
+package cucumberClass;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RegisterClass {
+	
+	@When("Enter the First Name")
+	public void enter_the_first_name() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Enter the Last Name")
+	public void enter_the_last_name() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Enter the Mobile Number")
+	public void enter_the_mobile_number() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Enter Zip Code") 
+	public void enter_zip_code() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Click the Register")
+	public void click_the_register() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Account will be successfully created")
+	public void account_will_be_successfully_created() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
---
+++ target/test-classes/Register.feature
@@ -0,0 +1,8 @@
+Feature: Register an Account for the User
+Scenario: Fill the Details of the User
+When Enter the First Name
+And Enter the Last Name
+And Enter the Mobile Number
+And Enter Zip Code
+And Click the Register 
+Then Account will be successfully created
